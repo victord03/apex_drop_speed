dc9a59a647b15e255a1d05cdb34b9d41489cb7eb
Victor Kaklamanis <vic.kaklamanis@gmail.com>
2023-05-30 01:59:40 +0300

    Simplified the problem. Created mock-up loop.

--- classes/Jumpmaster.py
+++ classes/Jumpmaster.py
@@ -1,12 +1,26 @@
 
 class JumpMaster:
+    direction: float  # 1 represents entirely horizontally; 0 entirely vertically
     speed: int
-    angle: int
-    position_x: int
-    position_y: int
 
     def __init__(self):
+        self.direction = 0.0
         self.speed = 0
-        self.angle = 0
-        self.position_x = 0
-        self.position_y = 100
+
+    def set_direction(self, direction: float):
+        self.direction = direction
+
+    def set_speed(self):
+        """min speed 9, max speed 12 (30% increase from min to max)"""
+
+        speed = int()
+
+        if 0.0 <= self.direction <= 0.33:
+            speed = 12
+        elif 0.33 < self.direction <= 0.66:
+            speed = 10
+        elif 0.66 < self.direction <= 1.0:
+            speed = 9
+
+        self.speed = speed
+
--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
 import math
 from classes.World_map import WorldMap
+from classes.Jumpmaster import JumpMaster
 import numpy as np
 
 def main():
 
+    jm = JumpMaster()
+
     map_dimensions = (100, 200)
     wm = WorldMap(map_dimensions)
 
-    print(round(np.rad2deg((1/math.sqrt(5))), 2))
+    diving = True
+    while diving:
+        progress = int()
+        direction = float(input())
 
-    angle_ratio = (1 / math.sqrt(5))
-    conversion_to_degrees = (180/math.pi)
-    print(round(angle_ratio * conversion_to_degrees, 2))
+        jm.set_direction(direction)
+        jm.set_speed()
+
+        diving = False
+
+
+
+    # print(round(np.rad2deg((1/math.sqrt(5))), 2))
+
+    # angle_ratio = (1 / math.sqrt(5))
+    # conversion_to_degrees = (180/math.pi)
+    # print(round(angle_ratio * conversion_to_degrees, 2))
 
 
 if __name__ == "__main__":
