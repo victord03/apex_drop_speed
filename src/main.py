e7e799f930b7cc0de58c00910bc611011b7a8ee2
Victor Kaklamanis <vic.kaklamanis@gmail.com>
2023-05-31 00:59:55 +0300

    Created a function to calculate the speed based on the angle of approach.

--- src/main.py
+++ src/main.py
@@ -1,49 +1,42 @@
-from classes.Jumpmaster import JumpMaster
+
+def calculate_speed(angle, minimum_speed, maximum_speed):
+    min_speed = minimum_speed
+    max_speed = maximum_speed
+
+    # Calculate the speed based on the angle. Max speed is achieved at 90 degrees, min at 0.
+    speed = min_speed + (max_speed - min_speed) * (90 - angle) / 90
+    return speed
+
 
 def main():
 
-    # jm = JumpMaster()
-
-    position = {"x": 0, "y": 10}
+    position = {'x': 0, 'y': 10}
     speed = 2
-    coefficient = 0.8
+    coefficient = 0.5
 
     positions_log = list()
     positions_log.append(tuple(position.values()))
 
     while True:
 
-        position["x"] += 1
-        position["y"] -= round(speed * coefficient, 1)
+        position['x'] += 1
+        position['y'] -= round(speed * coefficient, 1)
+
+        if position['y'] <= 0:
+            position['y'] = 0
+
+        # I want this to be checked before recording coordinates as if we have landed, we no longer track the free fall
+        ground_touched = position['y'] == 0
+        if ground_touched:
+            break
 
         positions_log.append(tuple(position.values()))
 
-        ground_touched = position["y"] <= 0
-        negative_y_next_iteration = position["y"] - round(speed * coefficient, 1) < 0
-        if ground_touched or negative_y_next_iteration:
-            break
 
     print(positions_log)
 
-"""diving = True
-    while diving:
-
-        progress = int()
-
-        direction = float(input())
-        jm.set_direction(direction)
-
-        jm.set_speed()
-
-        diving = False"""
 
 
-    # print(round(np.rad2deg((1/math.sqrt(5))), 2))
 
-    # angle_ratio = (1 / math.sqrt(5))
-    # conversion_to_degrees = (180/math.pi)
-    # print(round(angle_ratio * conversion_to_degrees, 2))
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
