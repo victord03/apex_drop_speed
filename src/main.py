38d4b78794a0862cc36b2dcf1d3cbe0017394f06
Victor Kaklamanis <vic.kaklamanis@gmail.com>
2023-06-05 01:12:55 +0300

    Trying the Map + Jumpmaster classes combo instead.

--- src/main.py
+++ src/main.py
@@ -1,41 +1,31 @@
 
-def calculate_speed(angle, minimum_speed, maximum_speed):
-    min_speed = minimum_speed
-    max_speed = maximum_speed
 
-    # Calculate the speed based on the angle. Max speed is achieved at 90 degrees, min at 0.
-    speed = min_speed + (max_speed - min_speed) * (90 - angle) / 90
-    return speed
+class JumpMaster:
+    coordinates: tuple[int, int]
+
+    def __init__(self):
+        self.coordinates = (0, 0)
+
+    def update_coordinates(self, new_coordinates: tuple[int, int]):
+        self.coordinates = new_coordinates
+
+
+
+class MapCoordinates:
+    dimensions: tuple[int, int]
+
+    def __init__(self, dimensions: tuple[int, int]):
+        self.dimensions = dimensions
+
+
 
 
 def main():
 
-    position = {'x': 0, 'y': 10}
-    speed = 2
-    coefficient = 0.5
+    initial_speed = 100
 
-    positions_log = list()
-    positions_log.append(tuple(position.values()))
-
-    while True:
-
-        position['x'] += 1
-        position['y'] -= round(speed * coefficient, 1)
-
-        if position['y'] <= 0:
-            position['y'] = 0
-
-        # I want this to be checked before recording coordinates as if we have landed, we no longer track the free fall
-        ground_touched = position['y'] == 0
-        if ground_touched:
-            break
-
-        positions_log.append(tuple(position.values()))
-
-
-    print(positions_log)
-
-
+    horizontal_speed = 50
+    vertical_speed = 75  # 30% increase from horizontal speed
 
 
 if __name__ == '__main__':
